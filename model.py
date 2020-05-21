ce638e7ef02b823885bd98a00bfe70c596ef8310
ahmetkerem <akeremtuberlin@gmail.com>
2020-05-21 10:52:48 +0200

    two more conv layers added, accuracy dropped

--- model.py
+++ model.py
@@ -37,14 +37,17 @@ def buildModel():
     model.add(Conv2D(64, (3,3), activation='relu'))    
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
 
-    # model.add(Conv2D(128, (3,3), activation='relu'))
     model.add(Conv2D(64, (3,3), activation='relu'))
     model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
-
-    # TODO: Add here the L2 layer
+    model.add(Dropout(0.25))
+    model.add(Conv2D(32, (3,3), activation='relu'))                                 
+    model.add(MaxPooling2D(pool_size=(3,3)))   
+    model.add(Conv2D(32, (3,3), activation='relu'))                                 
+    model.add(MaxPooling2D(pool_size=(3,3)))   
+    
+    #TODO: Add here the L2 layer
     buildDiscLoss()
 
     model.add(Flatten())
